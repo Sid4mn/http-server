ff29557ed673b42bc2fbf52e162be1b089b287fd
sid4mn <siddhantshettiwar@gmail.com>
2025-02-19 21:30:55 -0700

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,64 @@
 import socket  # noqa: F401
+import threading
+
+
+
+
+def handle_client(conn, addr):
+
+    with conn:
+        print(f"Connection accepted from {addr}")
+        request = conn.recv(4096).decode()
+        print(f"Request = {request}")
+
+        lines = request.split("\r\n")
+        if lines:
+            req_line = lines[0]
+            parts = req_line.split(" ")
+            print(f"Parts = {parts}")
+            
+            if len(parts) >= 2:
+                method, path = parts[0], parts[1]
+                print(f"Method = {method}, Path = {path}")
+
+                if path.startswith("/echo"):
+                    echo_str = path[len("/echo/"):]
+                    content_length = len(echo_str.encode())
+                    response = (
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/plain\r\n"
+                        f"Content-Length: {content_length}\r\n"
+                        "\r\n"
+                        f"{echo_str}"
+                    )
+
+                elif path.startswith("/user-agent"):
+                    user_agent_val = ""
+                    for line in lines[1:]:
+                        if line.lower().startswith("user-agent:"):
+                            user_agent_val = line[len("/user-agent"):].strip()
+                            break
+                    
+                    cont_length = len(user_agent_val.encode())
+                    response = (
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/plain\r\n"
+                        f"Content-Length: {cont_length}\r\n"
+                        "\r\n"
+                        f"{user_agent_val}"
+                    )
+                elif path == "/":
+                    response = "HTTP/1.1 200 OK\r\n\r\n"
+                else:
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            else:
+                response = "HTTP/1.1 400 Bad Request\r\n\r\n"
+
+
+        else:
+            response = "HTTP/1.1 400 Bad Request\r\n\r\n"
+
+        conn.send(response.encode())
 
 
 def main():
@@ -17,59 +77,8 @@ def main():
 
     while True:
         conn, addr = server_socket.accept()
-        with conn:
-            print(f"Connection accepted from {addr}")
-            request = conn.recv(4096).decode()
-            print(f"Request = {request}")
-
-            lines = request.split("\r\n")
-            if lines:
-                req_line = lines[0]
-                parts = req_line.split(" ")
-                print(f"Parts = {parts}")
-                
-                if len(parts) >= 2:
-                    method, path = parts[0], parts[1]
-                    print(f"Method = {method}, Path = {path}")
-
-                    if path.startswith("/echo"):
-                        echo_str = path[len("/echo/"):]
-                        content_length = len(echo_str.encode())
-                        response = (
-                            "HTTP/1.1 200 OK\r\n"
-                            "Content-Type: text/plain\r\n"
-                            f"Content-Length: {content_length}\r\n"
-                            "\r\n"
-                            f"{echo_str}"
-                        )
-
-                    elif path.startswith("/user-agent"):
-                        user_agent_val = ""
-                        for line in lines[1:]:
-                            if line.lower().startswith("user-agent:"):
-                                user_agent_val = line[len("/user-agent"):].strip()
-                                break
-                        
-                        cont_length = len(user_agent_val.encode())
-                        response = (
-                            "HTTP/1.1 200 OK\r\n"
-                            "Content-Type: text/plain\r\n"
-                            f"Content-Length: {cont_length}\r\n"
-                            "\r\n"
-                            f"{user_agent_val}"
-                        )
-                    elif path == "/":
-                        response = "HTTP/1.1 200 OK\r\n\r\n"
-                    else:
-                        response = "HTTP/1.1 404 Not Found\r\n\r\n"
-                else:
-                    response = "HTTP/1.1 400 Bad Request\r\n\r\n"
-
-
-            else:
-                response = "HTTP/1.1 400 Bad Request\r\n\r\n"
-
-            conn.send(response.encode())
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
 
 if __name__ == "__main__":
     main()
