93e802a1e327fd0c20bd1340b48f0600179de9ad
sid4mn <siddhantshettiwar@gmail.com>
2025-02-19 20:05:25 -0700

    [any message]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    # server_socket.accept() # wait for client
+    msg = "HTTP/1.1 200 OK\r\n\r\n"
+    server_socket.listen()
+    server = server_socket.accept()
+    server[0].send(msg.encode())
 
 
 if __name__ == "__main__":
